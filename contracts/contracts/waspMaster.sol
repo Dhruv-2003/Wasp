e100866c8dd10b209454353b39dd073e245caac8
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-06-05 18:42:39 +0530

    feat: hardhat initialised

---
+++ contracts/contracts/waspMaster.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Tasks
+// - fetch current price
+// - calculate ticks
+// - findTick
+// - mintPosition
+// - burnPosition
+
+contract waspMaster {
+
+}
