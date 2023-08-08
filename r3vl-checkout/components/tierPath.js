d3a5ac4726646fd2d9f49206048282c3be315d1b
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-08-09 01:06:45 +0530

    feat: Added the tier and Wallet system

--- r3vl-checkout/components/tierPath.js
+++ r3vl-checkout/components/tierPath.js
@@ -47,9 +47,9 @@ const TierPath = (props) => {
               )}
             </div>
             <WalletData
-              handleWalletAddresses={props.handleWalletAddresses}
-              handleDistributions={props.handleDistributions}
-              key={i}
+              tierList={tierList}
+              setTierList={setTierList}
+              tier={i}
             />
           </div>
         );
@@ -59,74 +59,76 @@ const TierPath = (props) => {
 };
 
 const WalletData = (props) => {
-  const [inputList, setInputList] = useState([
-    { walletAddress: "", amount: "" },
-  ]);
   const [amount, setAmount] = useState("");
 
   const handleInputChange = (e, index) => {
     const { name, value } = e.target;
-    const list = [...inputList];
-    list[index][name] = value;
-    setInputList(list);
+    const list = [...props.tierList];
+    list[props.tier][index][name] = value;
+    props.setTierList(list);
   };
 
   // handle click event of the Remove button
   const handleRemoveClick = (index) => {
-    const list = [...inputList];
-    list.splice(index, 1);
-    setInputList(list);
+    const list = [...props.tierList];
+    const tierList = props.tierList[props.tier];
+    tierList.splice(index, 1);
+    list[props.tier] = tierList;
+    props.setTierList(list);
   };
-
-  // console.log(inputList);
 
   // handle click event of the Add button
   const handleAddClick = () => {
-    setInputList([...inputList, { walletAddress: "", amount: "" }]);
+    const list = [...props.tierList];
+    const currentTierList = props.tierList[props.tier];
+    const newTierList = [...currentTierList, { walletAddress: "", amount: "" }];
+    list[props.tier] = newTierList;
+    props.setTierList(list);
   };
 
   return (
     <div className="mt-3 mx-3">
-      {inputList.map((x, i) => {
-        return (
-          <div className="flex flex-col w-full">
-            <input
-              name="walletAddress"
-              placeholder={`enter ${i + 1} walletAddress`}
-              value={x.walletAddress}
-              onChange={(e) => handleInputChange(e, i)}
-              className="bg-white w-full px-3 py-1 rounded-xl text-black"
-            />
-            <input
-              className="bg-white w-full px-3 py-1 rounded-xl text-black mt-4"
-              name="amount"
-              placeholder={`enter ${i + 1} amount`}
-              value={x.amount}
-              onChange={(e) => handleInputChange(e, i)}
-            />
-            <div className="">
-              {inputList.length !== 1 && (
-                <div className="w-full flex ">
+      {props.tierList[props.tier] &&
+        props.tierList[props.tier].map((x, i) => {
+          return (
+            <div className="flex flex-col w-full">
+              <input
+                name="walletAddress"
+                placeholder={`enter ${i + 1} walletAddress`}
+                value={x.walletAddress}
+                onChange={(e) => handleInputChange(e, i)}
+                className="bg-white w-full px-3 py-1 rounded-xl text-black"
+              />
+              <input
+                className="bg-white w-full px-3 py-1 rounded-xl text-black mt-4"
+                name="amount"
+                placeholder={`enter ${i + 1} amount`}
+                value={x.amount}
+                onChange={(e) => handleInputChange(e, i)}
+              />
+              <div className="">
+                {props.tierList[props.tier].length !== 1 && (
+                  <div className="w-full flex ">
+                    <button
+                      className="mt-3 mb-3 bg-yellow-500 text-black flex justify-center mx-auto px-4 py-1 rounded-xl"
+                      onClick={() => handleRemoveClick(i)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                )}
+                {props.tierList[props.tier].length - 1 === i && (
                   <button
-                    className="mt-3 mb-3 bg-yellow-500 text-black flex justify-center mx-auto px-4 py-1 rounded-xl"
-                    onClick={() => handleRemoveClick(i)}
+                    className="bg-yellow-500 text-black flex justify-center mt-4 w-full py-2 rounded-xl"
+                    onClick={handleAddClick}
                   >
-                    Remove
+                    Add
                   </button>
-                </div>
-              )}
-              {inputList.length - 1 === i && (
-                <button
-                  className="bg-yellow-500 text-black flex justify-center mt-4 w-full py-2 rounded-xl"
-                  onClick={handleAddClick}
-                >
-                  Add
-                </button>
-              )}
+                )}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 };
